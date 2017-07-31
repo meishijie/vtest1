bef0ba177f503f6acb28cb7328cb5b37570ae2ba
meishijie <719082908@qq.com>
2017-07-31 16:43:18 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <div class="navbar navbar-default navbar-fixed-top" >
+    <div class="container">
+      <div class="navbar-header" style="height:80px;">
+        <button class="navbar-toggle collapsed " style="margin-top:25px;" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand hidden-sm" style="padding:0; margin:0; margin-left:20px;" href="" onclick="_hmt.push(['_trackEvent', 'navbar', 'click', 'navbar-首页'])"><img src="../static/img/m/logo.jpg" /></a>
+        <div class="am-fl " style="line-height:24px;margin-top:30px;margin-left:100px; font-weight:bold;">Stock Code : 833031 </div>
+      </div>
+      <div class="navbar-collapse collapse" role="navigation">
+        <ul class="nav navbar-nav">
+          <li class="hidden-sm hidden-md"><a href="http://v2.bootcss.com/" target="_blank" onclick="_hmt.push(['_trackEvent', 'navbar', 'click', 'v2doc'])">Bootstrap2中文文档</a></li>
+          <li><a href="http://v3.bootcss.com/" target="_blank" onclick="_hmt.push(['_trackEvent', 'navbar', 'click', 'v3doc'])">Bootstrap3中文文档</a></li>
+          <li><a href="http://v4.bootcss.com/" target="_blank" onclick="_hmt.push(['_trackEvent', 'navbar', 'click', 'v4doc'])">Bootstrap 4.0 预览</a></li>
+          <li><a href="/p/lesscss/" target="_blank" onclick="_hmt.push(['_trackEvent', 'navbar', 'click', 'less'])">Less 教程</a></li>
+          <li><a href="http://www.jquery123.com/" target="_blank" onclick="_hmt.push(['_trackEvent', 'navbar', 'click', 'jquery'])">jQuery API</a></li>
+          <li><a href="http://expo.bootcss.com" target="_blank" onclick="_hmt.push(['_trackEvent', 'navbar', 'click', 'expo'])">网站实例</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right hidden-sm">
+          <li><a href="/about/" onclick="_hmt.push(['_trackEvent', 'navbar', 'click', 'about'])">关于</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  margin-top: 100px;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container">
+    <div class="breadcrumb-box">
+      <div class="am-container padding-left0">
+          <ol class="smallNav">
+              <li><a href="index.html">Home ></a></li>
+              <li class="am-active smallNavText">E-commerce Whole-channel Construction</li>
+          </ol>
+      </div>
+    </div>
+    <div class="projects-header page-header">
+        <h2>Bootstrap相关优质项目推荐</h2>
+        <p>这些项目或者是对Bootstrap进行了有益的补充，或者是基于Bootstrap开发的</p>
+        <img src="../../static/img/m/logo.jpg" />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'main',
+    data () {
+      return {
+        msg: '订单msg'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.projects-header {
+    width: 60%;
+    text-align: center;
+    margin: 60px 0 10px;
+    font-weight: 200;
+    margin-bottom: 40px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+.smallNav{
+  padding-left: 0;
+}
+.smallNav a{
+  color: #000;
+}
+.smallNavText{
+  color: #999999;
+}
+.breadcrumb-box {
+    border-bottom: 1px solid #e9e9e9;
+    margin-bottom: 30px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
